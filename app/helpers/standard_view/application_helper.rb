f0d2ba2128a9f9ab202de962e18787b904c96fd9
Eric Garside <garside@gmail.com>
2019-08-10 17:38:25 -0400

    Masthead

--- app/helpers/standard_view/application_helper.rb
+++ app/helpers/standard_view/application_helper.rb
@@ -5,8 +5,8 @@ module StandardView
     def icon_for(key, spin: false)
       localization_key = "icons.#{key}"
       definition = I18n.t(localization_key) if I18n.exists?(localization_key)
-      style = definition.try(:[], "style") || "s"
-      name = definition.try(:[], "name") || definition || "question"
+      name = definition.try(:[], :name) || definition || "question"
+      style = definition.try(:[], :style) || "s"
       content_tag(:i, "", class: "fa#{style} fa-#{name} #{"fa-spin" if spin}")
     end
   end
