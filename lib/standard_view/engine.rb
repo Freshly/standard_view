2626a5a02cec496327ecfa3b5be733f0b55e9d88
Eric Garside <garside@gmail.com>
2019-08-10 13:12:47 -0400

    Masthead

--- lib/standard_view/engine.rb
+++ lib/standard_view/engine.rb
@@ -3,5 +3,11 @@
 module StandardView
   class Engine < ::Rails::Engine
     isolate_namespace StandardView
+
+    initializer 'local_helper.action_controller' do
+      ActiveSupport.on_load :action_controller do
+        helper StandardView::ApplicationHelper
+      end
+    end
   end
 end
