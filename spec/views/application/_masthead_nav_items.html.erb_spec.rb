4f4a2fd62a1ea869619504e6d3e17fc42fa85b31
Eric Garside <garside@gmail.com>
2019-08-12 09:57:53 -0400

    Masthead

---
+++ spec/views/application/_masthead_nav_items.html.erb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "application/_masthead_nav_items.html.erb", type: :view do
+  # before do
+  #   render partial: "standard_view/breadcrumbs", locals: { material: material }
+  # end
+
+  it "needs specs"
+end
