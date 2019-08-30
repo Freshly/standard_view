ebd3d56fe7114cb26bf9a91406b874615a38f61f
Eric Garside <garside@gmail.com>
2019-08-30 14:35:41 -0400

    Index View Rendering

--- lib/standard_view.rb
+++ lib/standard_view.rb
@@ -5,5 +5,7 @@ require "standard_view/engine"
 require "material"
 require "will_paginate"
 require "will_paginate/active_record"
+require "will_paginate/view_helpers/link_renderer"
+require "will_paginate/view_helpers/action_view"
 
 module StandardView; end
