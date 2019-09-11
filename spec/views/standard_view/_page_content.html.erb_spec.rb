4a3f8cb16db11de8f8fea7a53ae2fefa4670181e
Eric Garside <garside@gmail.com>
2019-09-11 09:54:54 -0400

    List Item

---
+++ spec/views/standard_view/_page_content.html.erb_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "standard_view/_page_content.html.erb", type: :view do
+  # subject(:render_partial) { render partial: "standard_view/page_content", locals: {} }
+
+  it "needs specs"
+end
