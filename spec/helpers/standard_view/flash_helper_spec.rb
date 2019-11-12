5ed730796d77c9fd8e038c7be9d09312f3b7a7b1
Eric Garside <garside@gmail.com>
2019-11-12 07:50:40 -0500

    Flash Messages

---
+++ spec/helpers/standard_view/flash_helper_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe StandardView::FlashHelper, type: :helper do
+  describe "#alert_class_for_flash" do
+    it "needs specs"
+  end
+end
